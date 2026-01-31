97698e28055551934982c79285de443b2f422726
Abhinay Jangde <64852930+abhinayjangde@users.noreply.github.com>
2026-01-31 09:35:39 +0000

    basic upload pdf file

--- app/server.py
+++ app/server.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "healthy"}
+    return {"status": "healthy 👀"}
+
+@app.post("/upload")
+def upload_file(file: UploadFile = File(...)):
+
+    print(f"Received file: {file}")
+    return {"message": "Upload endpoint"}
 
